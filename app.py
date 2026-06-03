ec52813cc9667d84e9329f1512593897f1c1f511
mpawar91 <pawarmayuri15@gmail.com>
2026-06-03 13:05:35 -0700

    Update app.py
    change the hello message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from inside my very first container! My app is running and deployed on cloud!'
+    return 'Hello from inside my very first container! I can also deploy the changes!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
